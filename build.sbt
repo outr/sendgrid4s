e54d33bc3f46b9eb0b8d2ac03f14b3de03457637
Matt Hicks <matt@matthicks.com>
2023-05-17 11:41:01 -0500

    Release 1.0.6

--- build.sbt
+++ build.sbt
@@ -5,7 +5,7 @@ val scala213 = "2.13.10"
 
 name := "sendgrid4s"
 organization := "com.outr"
-version := "1.0.5"
+version := "1.0.6"
 
 scalaVersion := scala213
 crossScalaVersions := List(scala3, scala213)
@@ -35,5 +35,5 @@ outputStrategy := Some(StdoutOutput)
 fork := true
 
 libraryDependencies ++= Seq(
-  "com.outr" %% "spice-client-okhttp" % "0.0.30"
+  "com.outr" %% "spice-client-okhttp" % "0.0.31"
 )
--- src/main/scala/sendgrid/Sendgrid.scala
+++ src/main/scala/sendgrid/Sendgrid.scala
@@ -5,13 +5,16 @@ import fabric._
 import fabric.filter.RemoveEmptyFilter
 import fabric.io.JsonParser
 import fabric.rw._
-import spice.http.client.HttpClient
+import spice.http.client.{HttpClient, RetryManager}
 import spice.net._
+
+import scala.concurrent.duration.DurationInt
 
 case class Sendgrid(apiKey: String) {
   private lazy val client = HttpClient
     .url(url"https://api.sendgrid.com/v3/mail/send")
     .header("Authorization", s"Bearer $apiKey")
+    .retryManager(RetryManager.simple(5, 5.seconds))
 
   def send(message: Message): IO[Option[String]] = client
     .post
